030747f77561a97a50ca3f233f1d9bc5b7199ba1
Biniyamseid <Biniyamseid3@gmail.com>
2024-09-09 09:06:20 +0200

    Implemented JSD Surprisingness Calculation and Supporting Functions
    - Implemented mapper function for mapping multiple variables with Type.
    - `gen-jsd-surprisingness-rule` for generating rules based on JSD surprisingness.
    - `jsd-surprisingness-formula` for calculating the surprisingness of a pattern as the Jensen-Shannon Distance between its empirical truth value and its truth value estimate.
    - Implemented `gen-variables` function that accepts a parameter called `nary` and returns `n` variables with a specified prefix, which is needed in the JSD surprisingness calculation.

--- experiments/rules/conj-exp/main.py
+++ experiments/rules/conj-exp/main.py
@@ -13,6 +13,10 @@ from hyperon.base import Tokenizer, SExprParser
 from hyperon.ext import register_atoms, register_tokens
 import hyperonpy as hp
 
+
+mapping = {}
+    
+
 def combine_lists_op(metta: MeTTa, var1, var2):
     #($Y ($X ())) ($a  ($b ($c ())))
     input_str1 = str(var1)
@@ -146,6 +150,64 @@ def combine_lists(list1, list2):
     length = max(len(flat_list1), len(flat_list2))
     return combine_lists_recursive(flat_list1, flat_list2, length)
 
+def extract_variables_from_atom(atom):
+        """
+        Extracts variable names from a given atom. Variables are identified by a leading '$' symbol.
+        Variables containing '#' are ignored as they are considered invalid.
+
+        Parameters:
+        - atom: The atom from which to extract variable names.
+
+        Returns:
+        - A list of valid variable names extracted from the atom.
+        """
+        atom_str = str(atom)
+        pattern = r"\$[^\s\(\)]+"
+        variables = re.findall(pattern, atom_str)
+        return [var for var in variables if "#" not in var]
+    
+
+def generate_random_string(length=1):
+        """
+        Generates a random string of specified length composed of uppercase letters and digits.
+
+        Parameters:
+        - length (int): The length of the random string to generate. Default is 1.
+
+        Returns:
+        - A random string of uppercase letters and digits.
+        """
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+def generate_random_var(metta:MeTTa,a, b):
+        """
+        Generates a unique variable name based on the variables extracted from two atoms.
+        If a variable name for 'b' is already generated and stored in the mapping, it returns that variable.
+        Otherwise, it generates a new unique variable name, stores it in the mapping, and returns it.
+
+        Parameters:
+        - a: The first atom from which to extract variables.
+        - b: The second atom from which to extract variables and for which to generate a unique variable name.
+
+        Returns:
+        - A list containing the newly generated unique variable.
+        """
+        if str(a) in mapping:
+            atom = metta.parse_all(mapping[str(a)])
+        variables = extract_variables_from_atom(a)
+        variables_set = set(var[1:] for var in variables)
+        base_name = "$rn" + generate_random_string() + str(int(time.time()))
+
+        if base_name in variables_set:
+            i = 1
+            while f"{base_name}{i}" in variables_set:
+                i += 1
+            base_name = f"{base_name}{i}"
+
+        base_name = "("+base_name +")"
+        mapping[str(a)] = base_name
+        atom = metta.parse_all(base_name)
+
+        return atom
 # def unique_combinations(combinations, list1, list2):
 #     flat_list1 = flatten_list(list1)
 #     flat_list2 = flatten_list(list2)
@@ -176,96 +238,14 @@ def cnj_exp(metta):
         lambda list: format_list(metta, list), 
         ['Atom', 'Expression'], 
         unwrap=False)
+            # Define the operation atom with its parameters and function
+    genRandomVar = OperationAtom('genRandomVar', lambda var1, var2: (print(var1,var2),generate_random_var(metta,var1, var2))[1],
+                                      ['Atom', 'Atom', 'Expression'], 
+                                      unwrap=False)
     
     return {
         r"combine_lists": combineLists,
-        r"format_list": formatList
+        r"format_list": formatList,
+        r"genRandomVar": genRandomVar
     }
-
-
-@register_atoms(pass_metta=True)
-def rand_str(run_context):
-    """
-    This function registers an operation atom `generateRandomVar` that dynamically generates a unique variable name.
-    It ensures the generated variable name is not already used within the given context. The uniqueness is achieved by
-    combining a random string with the current timestamp, and checking against existing variables in the atom.
-
-    The operation atom `generateRandomVar` takes two atoms as input and generates a new variable that does not conflict
-    with any existing variable names extracted from these atoms.
-
-    
-
-    Parameters:
-    - run_context: The context in which this function is run, provided by the caller.
-
-    Returns:
-    - A dictionary mapping the operation name to the OperationAtom instance.
-    """
-
-    # Mapping to store generated variables to ensure uniqueness
-    mapping = {}
-    
-    def generate_random_string(length=1):
-        """
-        Generates a random string of specified length composed of uppercase letters and digits.
-
-        Parameters:
-        - length (int): The length of the random string to generate. Default is 1.
-
-        Returns:
-        - A random string of uppercase letters and digits.
-        """
-        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
-
-    def extract_variables_from_atom(atom):
-        """
-        Extracts variable names from a given atom. Variables are identified by a leading '$' symbol.
-        Variables containing '#' are ignored as they are considered invalid.
-
-        Parameters:
-        - atom: The atom from which to extract variable names.
-
-        Returns:
-        - A list of valid variable names extracted from the atom.
-        """
-        atom_str = str(atom)
-        pattern = r"\$[^\s\(\)]+"
-        variables = re.findall(pattern, atom_str)
-        return [var for var in variables if "#" not in var]
-
-    def generate_random_var(a, b):
-        """
-        Generates a unique variable name based on the variables extracted from two atoms.
-        If a variable name for 'b' is already generated and stored in the mapping, it returns that variable.
-        Otherwise, it generates a new unique variable name, stores it in the mapping, and returns it.
-
-        Parameters:
-        - a: The first atom from which to extract variables.
-        - b: The second atom from which to extract variables and for which to generate a unique variable name.
-
-        Returns:
-        - A list containing the newly generated unique variable.
-        """
-        variables = extract_variables_from_atom(a)
-        variables_set = set(var[1:] for var in variables)
-        base_name = "rn" + generate_random_string() + str(int(time.time()))
-
-        if base_name in variables_set:
-            i = 1
-            while f"{base_name}{i}" in variables_set:
-                i += 1
-            base_name = f"{base_name}{i}"
-
-        new_var = V(base_name)
-        mapping[str(b)] = new_var
-        return [new_var]
-
-    # Define the operation atom with its parameters and function
-    generateRandomVar = OperationAtom('generateRandomVar', lambda a, b: generate_random_var(a, b),
-                                      ['Atom', 'Atom', 'Variable'], unwrap=False)
-
-    return {
-        r"generateRandomVar": generateRandomVar
-    }
-
 
